f11ddc3613fc2b3ea4399c43f05354ebf03ceb5f
Ailun Shen <newhoopsdynasty@gmail.com>
2020-08-05 15:52:22 -0400

    Added Modal, finish up

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,8 +4,8 @@ const Title = () => {
   return (
     <div className="title">
       <h1>TheGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Go here! Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h2>Your Photo Gallery</h2>
+      <p>Below are the list of pictures stored on our server. So far authentication has not been implemented. Anyone can upload a photo. This shall be changed.</p>
     </div>
   )
 }
