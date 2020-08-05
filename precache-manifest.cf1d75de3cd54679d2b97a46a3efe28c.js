bc688aba1fd9d5d18501a30e79b5e8ce1528e45f
Ailun Shen <newhoopsdynasty@gmail.com>
2020-08-05 16:09:12 -0400

    Updates

---
+++ precache-manifest.cf1d75de3cd54679d2b97a46a3efe28c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd6a8b3ffb79372749d4efb4b373345",
+    "url": "/thegram/index.html"
+  },
+  {
+    "revision": "6b6c5e087c6137fa26a0",
+    "url": "/thegram/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "c7632145c73359b9ee94",
+    "url": "/thegram/static/js/2.0b660342.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/thegram/static/js/2.0b660342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b6c5e087c6137fa26a0",
+    "url": "/thegram/static/js/main.de889ef5.chunk.js"
+  },
+  {
+    "revision": "08bb5e5228635a36df55",
+    "url": "/thegram/static/js/runtime-main.940b16a5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/thegram/precache-manifest.cf1d75de3cd54679d2b97a46a3efe28c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thegram/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
